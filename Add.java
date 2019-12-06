72020f51249adae903676d35116bf2df6b02afb0
Kalyani Patil <Kalyani.Patil@cuelogic.local>
2019-12-06 21:01:17 +0530

    Fix the absolute value change for subtraction

--- Add.java
+++ Add.java
@@ -1,6 +1,6 @@
 public class Add{
     public static void main(String[] args) {
-        int a=4,b=5,sum,sub;
+        int a=4,b=2,sum,sub;
         sum=add(a,b);
         sub=subtraction(a,b);
         System.out.println("Addition of two numbers is : "+sum);
@@ -10,6 +10,6 @@ static int add( int x, int y){
 	return x+y;
 }
 static int subtraction(int x,int y){
-    return y-x;
+    return Math.abs(y-x);
     }
 }
