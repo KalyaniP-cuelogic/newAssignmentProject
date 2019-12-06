f15d584781edd3fd63774c8db53cd5a088f13dac
Kalyani Patil <Kalyani.Patil@cuelogic.local>
2019-12-06 16:34:23 +0530

    Add the multiplication method

--- Add.java
+++ Add.java
@@ -1,7 +1,13 @@
 public class Add{
     public static void main(String[] args) {
-        int a=4,b=5,sum;
+        int a=4,b=5,sum,mul;
         sum = a+b;
         System.out.println("Summation is" +" " +sum);
+        mul=multiplication(a,b);
+        System.out.println("Multiplication of two nubmers is "+mul);
+    }
+
+    static int multiplication(int x,int y){
+        return x*y;
     }
 }
